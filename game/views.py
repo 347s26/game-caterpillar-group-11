efaf406857018bd1970c9f446337522d4fd58557
Will Upright <uprighwx@dukes.jmu.edu>
2026-02-23 18:35:27 -0500

    Stopped tracking sqlite file and added home page

--- game/views.py
+++ game/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
 
-# Create your views here.
+from .models import Game, Move, Position
+
+def index(request):
+    """View function for home page"""
+
+    num_games = Game.objects.count()
+    num_active_games = Game.objects.filter(is_game_over=False).count()
+    num_finished_games = Game.objects.filter(is_game_over=True).count()
+
+    context = {
+        "num_games": num_games,
+        "num_active_games": num_active_games,
+        "num_finished_games": num_finished_games,
+    }
+
+    return render(request, "index.html", context)
